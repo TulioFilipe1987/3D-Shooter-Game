27cbafe2d91dc97da31800ce564993d873e675ac
TulioFilipe1987 <tulio.ruy@gmail.com>
2022-07-03 14:32:33 -0300

    Game of shoot 3D

---
+++ Assets/Scenes/Script/Camera Script/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed = 20;
+    private Transform playerTarget;
+
+    void Awake(){
+        playerTarget = GameObject.Find("Robot").transform;
+        
+    }
+
+ 
+    void Update(){
+        if (playerTarget){
+            Vector3 start = transform.position;
+            Vector3 pos = new Vector3(playerTarget.position.x, playerTarget.position.y + 3f,
+                playerTarget.position.z);
+
+            Vector3 end = Vector3.MoveTowards(start, pos, followSpeed * Time.deltaTime); // a soma dos 2 de cima 
+
+            end.z = start.z;
+            transform.position = end;
+
+        }
+
+        
+    }
+}// class
---
+++ Assets/Scenes/Script/Enemy Script/CannonScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour{
+
+    private Transform player;
+    public GameObject explosion;
+
+    
+
+
+    void Start(){
+        player = GameObject.Find("Robot").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (player){
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(player.position - transform.position),
+                2f * Time.deltaTime);
+        }
+        
+    }
+
+    void Damage(){
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+
+    void OnParticleCollision(GameObject target){ //aqui é se o tiro bater e nao o missel
+        if (target.name == "Muzzle"){
+            Damage();
+
+        }
+    }
+
+}//calsss
